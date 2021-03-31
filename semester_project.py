906cf995c491d98e79ac9b4073e052c6b08c109c
Mario Castillo <mcastillo130@gmail.com>
2021-03-30 22:34:35 -0400

    I added a class for a person with one attribute

---
+++ semester_project.py
@@ -0,0 +1,11 @@
+""" A simple semester project for spring 2021 """:
+
+class Person:
+    """ This will be user or the person using
+    our program
+
+    Attributes:
+    person's major(str): the major that the 
+    student is considering.
+    
+    """
